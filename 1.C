328bcdfad92fc0fe34a5e25dc9af95f2bd237fa6
蒋坤 <987062571@qq.com>
2020-04-04 17:47:05 +0800

    2

--- 1.C
+++ 1.C
@@ -292,3 +292,222 @@ int Calculation ( char *str, int i )
 	return 1;
 
 }
+//生成算术表达式
+int Random ( int m )  //随机生成给定范围内一个数字
+{
+	int a;
+	srand ( ( unsigned ) time ( NULL ) );
+	Sleep ( 1000 );
+	a = rand() % m + 1;
+	return a;
+}
+
+
+char Symbol()  //随机生成‘+、-、*、/’运算符其中一个
+{
+	int a, b = 0;
+	char x;
+	a = Random ( 4 );
+	do
+	{
+
+
+		switch ( a )
+		{
+		case 1:
+			x = '+';
+			break;
+
+		case 2:
+			x = '-';
+			break;
+
+		case 3:
+			x = '*';
+			break;
+
+		case 4:
+			x = '/';
+			break;
+
+		default:
+			b = 1;
+
+		}
+	}
+	while ( b );
+	return x;
+}
+
+
+
+char Brackets ( int co, int ad )  //根据给定参数判断并随机生成‘（、）’小括号
+{
+	int a;
+	char x = '#';
+	switch ( ad )
+	{
+	case 0:
+	{
+		a = Random ( 2 );
+		switch ( a )
+		{
+		case 1:
+			x = '(';
+			break;
+
+		case 2:
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	case 1:
+	{
+		a = Random ( 2 );
+		if ( co != 0 )
+			switch ( a )
+			{
+			case 1:
+				x = ')';
+				break;
+
+			case 2:
+				break;
+
+			default:
+				break;
+			}
+		else
+			break;
+	}
+	}
+
+	return x;
+}
+
+
+int File_write ( char *a, int i )  //将生成的算术表达式写入Eercises.txt文件
+{
+	int j = 0;
+	FILE *fpWrite = fopen ( "Exercises.txt", "a" );
+	if ( fpWrite == NULL )
+	{
+		return 0;
+	}
+	fprintf ( fpWrite, "topic%d:\t", i );
+	while ( a[j] != NULL )
+	{
+		if ( a[j] == '+' || a[j] == '-' || a[j] == '*' || a[j] == '/' || a[j] == '=' )
+			fprintf ( fpWrite, " %c ", a[j++] );
+		else
+			fprintf ( fpWrite, "%c", a[j++] );
+	}
+	fprintf ( fpWrite, " = " );
+	fprintf ( fpWrite, "\n" );
+	fclose ( fpWrite );
+	return 1;
+}
+
+
+
+void Arithmetic ( int i, int n, int m )  //生成算术表达式主体函数，n为最大运算符个数，m为数值范围，i为题目编号
+{
+	char a[250] = {'0'};  //定义一个字符数组存放算术表达式
+	int x, p, add, rge, rng_num, count = 0, j = 0;  //count作为计数器，记录生成左括号的个数
+	rge = Random ( n );
+	for ( int k = 0; k < rge; k++ )  //循环生成算术表达式
+	{
+		add = 0;  //add作为标志值，影响Brackets()小括号生成函数内部执行
+		a[j] = Brackets ( count, add );  //随机生成左括号
+		if ( a[j] == '(' )
+		{
+			j++;
+			count++;
+		}
+
+		rng_num = Random ( m );  //生成一个参与表达式的数值
+		x = 1;
+		p = 0;
+		while ( rng_num / x )
+		{
+			x *= 10;
+		}
+		x /= 10;
+
+		while ( x )
+		{
+			a[j++] = rng_num / x + '0';
+			rng_num = rng_num % x;
+			x /= 10;
+			p++;
+		}
+
+		if ( a[j - 1 - p] != '(' )
+		{
+			add = 1;
+			a[j] = Brackets ( count, add );
+			if ( a[j] == ')' )
+			{
+				j++;
+				count--;
+			}
+		}
+
+		a[j++] = Symbol();  //随机生成一个运算符
+
+	}
+
+
+
+	rng_num = Random ( m );  //生成一个随机结尾数值
+	x = 1;
+	while ( rng_num / x )
+	{
+		x *= 10;
+	}
+	x /= 10;
+
+	while ( x )
+	{
+		a[j++] = rng_num / x + '0';
+		rng_num = rng_num % x;
+		x /= 10;
+	}
+
+	while ( count )  //根据count值是否为0，判断所有左括号均有右括号对应
+	{
+
+		a[j++] = ')';
+		count--;
+	}
+	while ( File_write ( a, i ) == 0 );  //写入生成的算术表达式
+	while ( Calculation ( a, i ) == 0 );  //计算结果
+}
+
+
+
+
+//主函数
+int main()
+{
+	int n, m, s, i;
+	printf ( "请输入算术表达式最多含运算符的个数：" );
+	scanf ( "%d,%d,%d", &n );
+	printf ( "请输入算术表达式内数值的范围：" );
+	scanf ( "%d", &m );
+	printf ( "请输入生成算术表达式的数量：" );
+	scanf ( "%d", &s );
+	FILE *fp1, *fp2;
+	fp1 = fopen ( "Exercises.txt", "w" );
+	fclose ( fp1 );
+	fp2 = fopen ( "Answers.txt", "w" );
+	fclose ( fp2 );
+	for ( i = 1; i <= s; i++ )
+	{
+		Arithmetic ( i, n, m );
+	}
+	return 0;
+}
